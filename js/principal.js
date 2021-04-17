0a4cca9c7d4dab0b4ffd441798004dfcf1f46530
Jhonatas Campos <68759724+jhonatascampos@users.noreply.github.com>
2021-04-17 14:47:20 -0300

    Upload operadores

---
+++ js/principal.js
@@ -0,0 +1,49 @@
+console.log("Fui carregado de um arquivo externo");
+
+var titulo = document.querySelector(".titulo");
+
+titulo.textContent = "Checklist Nutricionista";
+
+var pacientes = document.querySelectorAll(".paciente");
+
+for (var i = 0; i < pacientes.length; i++) {
+
+    var paciente = pacientes[i];
+
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+
+    var tdImc = paciente.querySelector(".info-imc");
+
+    var pesoEhValido = true;
+    var alturaEhValida = true;
+
+    if (peso <= 0 || peso >= 1000) {
+        console.log("Peso inválido!");
+        pesoEhValido = false;
+        tdImc.textContent = "Peso inválido";
+        paciente.style.backgroundColor = "orange";
+    }
+
+    if (altura <= 0 || altura >= 3.00) {
+        console.log("Altura inválida!");
+        alturaEhValida = false;
+        tdImc.textContent = "Altura inválida";
+    }
+
+  if (alturaEhValida && pesoEhValido) {
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    }
+}
+
+titulo.addEventListener("click", function (){
+console.log("Posso chamar uma função anonima.");
+});
+
+function mostraMensagem(){
+  console.log("Olá, fui clicado!");
+}
